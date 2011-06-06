492f73d8ccef688a333c828b8ead16b0b272f835
Jabba Laci <jabba.laci@gmail.com>
2011-06-06 03:12:44 -0400

    wallpaper modes documented

--- wallpapers/helper/gnome.py
+++ wallpapers/helper/gnome.py
@@ -26,10 +26,11 @@ def set_wallpaper_xml():
 
 
 def set_wallpaper_image(img, mode='stretched'):
-    """Set the given file as wallpaper."""
+    """Set the given file as wallpaper.
+    
+    Possible modes: wallpaper, centered, scaled, stretched."""
     cmd1 = "gconftool-2 --type=string --set /desktop/gnome/background/picture_options {mode}".format(mode=mode)
     cmd2 = "gconftool-2 --type=string --set /desktop/gnome/background/picture_filename {img}".format(img=img)
     
     call(shlex.split(cmd1), stdout=PIPE)
     call(shlex.split(cmd2), stdout=PIPE)
-    
